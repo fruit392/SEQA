e43f6ca8dd67fc249d8a5bbbd810a0fdb41606da
fruit392 <53462909+fruit392@users.noreply.github.com>
2025-04-10 19:14:24 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys                                              
 from selenium.webdriver.firefox.service import Service                                      
-from selenium.webdriver import FirefoxOptions
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pytest
